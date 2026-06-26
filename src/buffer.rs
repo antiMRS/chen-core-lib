8edf429991ca53ff74f5723a71fe684bfe205e3e
antiMRS <mine2009_1p@inbox.ru>
2026-06-26 11:06:13 +0300

    Deleted unused

--- src/buffer.rs
+++ src/buffer.rs
@@ -65,36 +65,6 @@ impl<T: Default + Copy> Buffer<T> {
     }
 }
 
-impl<T: Clone + Copy + PartialEq + Default> Buffer<T> {
-    pub fn draw_empty(
-        &mut self,
-        size: &Size,
-        other: &Self,
-        other_size: &Size,
-        x: usize,
-        y: usize,
-        empty: T,
-    ) {
-        let w = size.w() as i64;
-        let h = size.h() as i64;
-
-        for i in 0..other.buf.len() {
-            let local_pos = Position::from_flat(i as i64, other_size);
-            let target_x = local_pos.x() + x as i64;
-            let target_y = local_pos.y() + y as i64;
-            if target_x < 0 || target_y < 0 || target_x >= w || target_y >= h {
-                continue;
-            }
-            let target_idx = Position::flattened(size, target_x, target_y) as usize;
-            let src_idx = i;
-
-            if other.buf[src_idx] != empty {
-                self.buf[target_idx] = other.buf[src_idx];
-            }
-        }
-    }
-}
-
 impl<T: Clone + Copy + Default> Buffer<T> {
     pub fn geometry_draw(&mut self, size: &Size, geom: &Geometry, pos: &Position, what: T) {
         let verts = geom.vertices_global(pos);
