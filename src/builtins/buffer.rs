9a351fb2d3084e6c3097380414fc3b2986dff7df
antiMRS <mine2009_1p@inbox.ru>
2026-07-19 14:19:02 +0300

    Fixed error while compiling with use_gui

--- src/builtins/buffer.rs
+++ src/builtins/buffer.rs
@@ -14,9 +14,10 @@ impl<T: Default + Copy> Buffer<T> {
         }
     }
 
-    //pub fn len(&self) -> usize {
-    //    self.buf.len()
-    //}
+    #[allow(unused)]
+    pub fn len(&self) -> usize {
+        self.buf.len()
+    }
 
     pub fn new_filled(w: usize, h: usize, what: T) -> Self {
         Self {
